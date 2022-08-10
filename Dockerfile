c16cb15470a2c4491a248231c3703838b17ad675
yut7 <yut7@masi-44.vuds.vanderbilt.edu>
2022-08-10 11:49:08 -0500

    Docker container

---
+++ Dockerfile
@@ -0,0 +1,114 @@
+FROM ubuntu:bionic-20220427 as builder
+
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+                    apt-transport-https \
+                    bc \
+                    build-essential \
+                    ca-certificates \
+                    gnupg \
+                    ninja-build \
+                    git \
+                    software-properties-common \
+                    wget \
+                    zlib1g-dev
+
+RUN wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null \
+    | apt-key add - \
+  && apt-add-repository -y 'deb https://apt.kitware.com/ubuntu/ bionic main' \
+  && apt-get update \
+  && apt-get -y install cmake=3.18.3-0kitware1 cmake-data=3.18.3-0kitware1
+
+
+RUN git clone https://github.com/ANTsX/ANTs.git \
+    && mkdir -p /tmp/ants/build \
+    && mv ANTs /tmp/ants/source \
+    && cd /tmp/ants/build \
+    && mkdir -p /opt/ants \
+    && git config --global url."https://".insteadOf git:// \
+    && cmake \
+      -GNinja \
+      -DBUILD_TESTING=ON \
+      -DRUN_LONG_TESTS=OFF \
+      -DRUN_SHORT_TESTS=ON \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_INSTALL_PREFIX=/opt/ants \
+      /tmp/ants/source \
+    && cmake --build . --parallel \
+    && cd ANTS-build \
+    && cmake --install .
+
+ENV ANTSPATH="/opt/ants/bin" \
+    PATH="/opt/ants/bin:$PATH" \
+    LD_LIBRARY_PATH="/opt/ants/lib:$LD_LIBRARY_PATH"
+
+FROM ubuntu:20.04
+ENV PATH="/root/miniconda3/bin:${PATH}"
+ARG PATH="/root/miniconda3/bin:${PATH}"
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    git \
+    python \
+    tar \
+    bzip2 \
+    libssl-dev \
+    dc \
+    perl \
+    tcsh \
+    unzip \
+    bc \
+    zlib1g-dev \
+    wget &&\
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# miniconda
+RUN wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh && \
+    mkdir /root/.conda && \
+    bash Miniconda3-py39_4.12.0-Linux-x86_64.sh -b && \
+    rm -f Miniconda3-py39_4.12.0-Linux-x86_64.sh
+
+RUN conda install -c mrtrix3 mrtrix3 && \
+    conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch && \
+    conda install -c conda-forge nibabel && \
+    conda install numpy
+    
+#fsl
+RUN wget https://fsl.fmrib.ox.ac.uk/fsldownloads/fslinstaller.py && \
+    python fslinstaller.py -d /opt/fsl -V 6.0.4
+
+# Install Convert3D (stable build 1.0.0)
+RUN wget -O c3d-1.0.0-Linux-x86_64.tar.gz "https://downloads.sourceforge.net/project/c3d/c3d/1.0.0/c3d-1.0.0-Linux-x86_64.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fc3d%2Ffiles%2Fc3d%2F1.0.0%2Fc3d-1.0.0-Linux-x86_64.tar.gz%2Fdownload&ts=1571934949" && \
+    tar -xf c3d-1.0.0-Linux-x86_64.tar.gz -C /opt/ && \
+    rm c3d-1.0.0-Linux-x86_64.tar.gz
+
+# freesurfer
+RUN wget https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/6.0.0/freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz && \
+    tar -C /opt -xzvf freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz && \
+    echo "This is a dummy license file. Please bind your freesurfer license file to this file." > /opt/freesurfer/license.txt &&\
+    rm freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz && \
+    cd /
+
+# ANTs
+COPY --from=builder /opt/ants /opt/ants
+
+ENV FSLDIR=/opt/fsl
+ENV PATH=${FSLDIR}/bin:${PATH}
+
+ENV ANTSPATH="/opt/ants/bin" \
+    PATH="/opt/ants/bin:$PATH" \
+    LD_LIBRARY_PATH="/opt/ants/lib:$LD_LIBRARY_PATH"
+
+ENV PATH="/opt/c3d-1.0.0-Linux-x86_64/bin:$PATH"
+
+ENV FREESURFER_HOME=/opt/freesurfer
+
+RUN mkdir /home/INPUTS && \
+    mkdir /home/OUTPUTS
+
+COPY src /home
+RUN chmod 777 /home/pipeline.sh
+
+SHELL ["/bin/bash", "-c"]
+ENTRYPOINT ["/home/pipeline.sh"]
---
+++ src/pipeline.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+TOPUP=true
+motion_corrected=false
+
+for arg in "$@"
+do
+    case $arg in
+        -nt|--notopup)
+            TOPUP=false
+	        ;;
+        -mc|--motion_corrected)
+            motion_corrected=true
+            ;;
+    esac
+done
+
+# setup freesurfer
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+cd /home/INPUTS
+
+# check file existence
+if [ ! -f T1.nii.gz ]; then
+    echo "T1.nii.gz not found"
+    exit 1
+fi
+
+if [ ! -f BOLD_d.nii.gz ]; then
+    echo "BOLD_d.nii.gz not found"
+    exit 1
+fi
+
+INPUTS_PATH=/home/INPUTS
+RESULTS_PATH=/home/OUTPUTS
+T1_ATLAS_PATH=/home/mni_icbm152_t1_tal_nlin_asym_09c.nii
+T1_ATLAS_2_5_PATH=/home/mni_icbm152_t1_tal_nlin_asym_09c_2_5.nii
+model_path=/home/Models
+T1_PATH=$INPUTS_PATH/T1.nii.gz
+BOLD_PATH=$RESULTS_PATH/BOLD_d.nii.gz
+BOLD_d_mc=$RESULTS_PATH/BOLD_d_mc.nii.gz
+BOLD_d_3D=$RESULTS_PATH/BOLD_d_3D.nii.gz
+
+
+# always use sform
+cp BOLD_d.nii.gz $BOLD_PATH
+if [[ $(fslorient -getsformcode $BOLD_PATH) -eq 1 ]] && [[ $(fslorient -getqformcode $BOLD_PATH) -eq 1 ]]; then
+    fslorient -setqformcode 0 $BOLD_PATH
+fi
+
+dimension=$(mrinfo $BOLD_PATH -ndim)
+
+if [[ $dimension -eq 3 ]]; then
+    cp $BOLD_PATH $BOLD_d_mc
+    cp $BOLD_d_mc $BOLD_d_3D
+elif [[ $dimension -eq 4 ]]; then
+    if $motion_corrected; then
+        cp $BOLD_PATH $BOLD_d_mc
+        mrmath $BOLD_d_mc mean $BOLD_d_3D -axis 3 -force
+    else
+        mcflirt -in $BOLD_PATH -meanvol -out $RESULTS_PATH/rBOLD -plots
+        mv $RESULTS_PATH/rBOLD.nii.gz $BOLD_d_mc
+        mv $RESULTS_PATH/rBOLD_mean_reg.nii.gz $BOLD_d_3D
+    fi
+else
+    echo "BOLD_d.nii.gz Incorrect Dimension"
+fi
+
+# Normalize T1
+echo -------
+echo Normalizing T1
+mri_convert $T1_PATH ${RESULTS_PATH}/T1.mgz
+mri_nu_correct.mni --i ${RESULTS_PATH}/T1.mgz --o ${RESULTS_PATH}/T1_N3.mgz --n 2
+mri_convert ${RESULTS_PATH}/T1_N3.mgz ${RESULTS_PATH}/T1_N3.nii.gz
+mri_normalize -g 1 -mprage ${RESULTS_PATH}/T1_N3.mgz ${RESULTS_PATH}/T1_norm.mgz
+mri_convert ${RESULTS_PATH}/T1_norm.mgz ${RESULTS_PATH}/T1_norm.nii.gz
+
+# Skull strip T1
+echo -------
+echo Skull stripping T1
+bet $T1_PATH ${RESULTS_PATH}/T1_mask.nii.gz -R
+
+# epi_reg distorted b0 to T1; wont be perfect since B0 is distorted
+echo -------
+echo epi_reg distorted b0 to T1
+epi_reg --epi=$BOLD_d_3D --t1=$T1_PATH --t1brain=${RESULTS_PATH}/T1_mask.nii.gz --out=${RESULTS_PATH}/epi_reg_d
+
+# Convert FSL transform to ANTS transform
+echo -------
+echo converting FSL transform to ANTS transform
+c3d_affine_tool -ref $T1_PATH -src $BOLD_d_3D ${RESULTS_PATH}/epi_reg_d.mat -fsl2ras -oitk ${RESULTS_PATH}/epi_reg_d_ANTS.txt
+
+# ANTs register T1 to atlas
+echo -------
+echo ANTS syn registration
+antsRegistrationSyNQuick.sh -d 3 -f $T1_ATLAS_PATH -m $T1_PATH -o ${RESULTS_PATH}/ANTS
+
+# Apply linear transform to normalized T1 to get it into atlas space
+echo -------
+echo Apply linear transform to T1
+antsApplyTransforms -d 3 -i ${RESULTS_PATH}/T1_norm.nii.gz -r $T1_ATLAS_2_5_PATH -n BSpline -t ${RESULTS_PATH}/ANTS0GenericAffine.mat -o ${RESULTS_PATH}/T1_norm_lin_atlas_2_5.nii.gz
+
+# Apply linear transform to N3 T1 to get it into atlas space
+echo -------
+echo Apply linear transform to T1 N3
+antsApplyTransforms -d 3 -i ${RESULTS_PATH}/T1_N3.nii.gz -r $T1_ATLAS_2_5_PATH -n BSpline -t ${RESULTS_PATH}/ANTS0GenericAffine.mat -o ${RESULTS_PATH}/T1_N3_lin_atlas_2_5.nii.gz
+
+# Apply linear transform to distorted b0 to get it into atlas space
+echo -------
+echo Apply linear transform to distorted b0
+antsApplyTransforms -d 3 -i $BOLD_d_3D -r $T1_ATLAS_2_5_PATH -n BSpline -t ${RESULTS_PATH}/ANTS0GenericAffine.mat -t ${RESULTS_PATH}/epi_reg_d_ANTS.txt -o ${RESULTS_PATH}/b0_d_lin_atlas_2_5.nii.gz
+
+cd $RESULTS_PATH
+# Run inference
+NUM_FOLDS=5
+for i in $(seq 1 $NUM_FOLDS);
+do 
+  echo Performing inference on FOLD: "$i"
+  python3 /home/inference.py T1_norm_lin_atlas_2_5.nii.gz b0_d_lin_atlas_2_5.nii.gz BOLD_s_3D_lin_atlas_2_5_FOLD_$i.nii.gz $model_path/num_fold_${i}_total_folds_5_seed_1_num_epochs_120_lr_0.0001_betas_\(0.9,\ 0.999\)_weight_decay_1e-05_num_epoch_*.pth
+done
+
+# Take mean
+echo Taking ensemble average
+fslmerge -t BOLD_s_3D_lin_atlas_2_5_merged.nii.gz BOLD_s_3D_lin_atlas_2_5_FOLD_*.nii.gz
+fslmaths BOLD_s_3D_lin_atlas_2_5_merged.nii.gz -Tmean BOLD_s_3D_lin_atlas_2_5.nii.gz
+
+# Apply inverse xform to undistorted b0
+echo Applying inverse xform to undistorted b0
+antsApplyTransforms -d 3 -i BOLD_s_3D_lin_atlas_2_5.nii.gz -r $BOLD_d_3D -n BSpline -t [epi_reg_d_ANTS.txt,1] -t [ANTS0GenericAffine.mat,1] -o BOLD_s_3D.nii.gz
+
+# Smooth image
+echo Applying slight smoothing to distorted b0
+fslmaths $BOLD_d_3D -s 1.15 BOLD_d_3D_smoothed.nii.gz
+
+fslmerge -t BOLD_all BOLD_d_3D_smoothed.nii.gz BOLD_s_3D.nii.gz
+
+if $TOPUP; then
+    echo -e "0 1 0 1\n0 1 0 0" > acqparams.txt
+    topup -v --imain=BOLD_all.nii.gz --datain=acqparams.txt --config=b02b0.cnf --iout=BOLD_all_topup --out=topup_results --subsamp=1,1,1,1,1,1,1,1,1 --miter=10,10,10,10,10,20,20,30,30 --lambda=0.00033,0.000067,0.0000067,0.000001,0.00000033,0.000000033,0.0000000033,0.000000000033,0.00000000000067 --scale=0
+    applytopup --imain=$BOLD_d_mc --datain=acqparams.txt --inindex=1 --topup=topup_results --out=BOLD_u --method=jac
+
+    dimension=$(mrinfo BOLD_u.nii.gz -ndim)
+    echo $dimension
+
+    if [[ $dimension -eq 4 ]]; then
+        mrmath BOLD_u.nii.gz mean BOLD_u_3D.nii.gz -axis 3 -force
+    elif [[ $dimension -eq 3 ]]; then
+        cp BOLD_u.nii.gz BOLD_u_3D.nii.gz
+    else
+        echo "BOLD_u.nii.gz doesn't have correct dimension"
+    fi
+fi
